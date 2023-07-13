164165560c365efb90cf36445044ce6d946d95b6
Reghardt <reghardt1000@gmail.com>
2023-07-13 22:56:31 +0200

    tailwind cdn changes

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from "$fresh/server.ts";
-import { Head } from "$fresh/runtime.ts";
+import { Head, asset } from "$fresh/runtime.ts";
 
 export default function App({ Component }: AppProps) {
   return (
     <html>
         <Head>
-            <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+          {/* <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script> */}
+          <link href={ asset("/output.css") } rel="stylesheet" />
+          <link rel="stylesheet" href="https://esm.sh/v128/@tailwindcss/typography@0.5.0/dist/typography.min.css"/>
         </Head>
         <body>
+
             <Component />
         </body>
       
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+// import typography from "tailwindcss/typography";
+
+console.log(typography)
+
 // /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["islands/**.{js,jsx,tsx}","routes/**.{js,jsx,tsx}","components/**.{js,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+// import forms from "tailwindcss/forms"
+
+
 
