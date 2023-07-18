538943b70ef8a127ba279802a208098bf62bacfa
Reghardt <reghardt1000@gmail.com>
2023-07-18 15:31:14 +0200

    Article improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('./node_modules/tailwindcss/types/config.d.ts').Config} */
 module.exports = {
   content: ["./routes/**/*.tsx", "./islands/**/*.tsx", "./components/**/*.tsx"],
   theme: {
