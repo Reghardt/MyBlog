e860cb73da9df0dfad30169c83ddabb15005646b
Reghardt <reghardt1000@gmail.com>
2023-07-18 17:50:38 +0200

    Import + article changes

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,6 @@
-//only used during development
-/** @type {import("npm:prettier").Options} */
+/** @type {import("./node_modules/@types/prettier/index.d.ts").Options} */
 module.exports = {
+  // tabWidth: 2,
   tabWidth: 2,
   useTabs: false,
   plugins: ["./node_modules/prettier-plugin-tailwindcss/dist/index.js"],
