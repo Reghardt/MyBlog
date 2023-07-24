7c60a8f89e573701d130b04536224509c25b1aea
Reghardt <reghardt1000@gmail.com>
2023-07-24 12:53:36 +0200

    like counter updated + why blog changes

--- islands/LikeArticle.tsx
+++ islands/LikeArticle.tsx
@@ -1,22 +1,36 @@
 import IconHeart from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/heart.tsx";
 import { signal } from "@preact/signals";
 import ky from "ky";
+import { useEffect } from "https://esm.sh/v128/preact@10.15.1/hooks/src/index.js";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 
 const likes = signal(0);
 
-export default function LikeArticle(props: { url_title: string }) {
-  async function like() {
-    const res = await ky
-      .post("/api/like", { json: { url_title: props.url_title } })
-      .json<{ likes: number }>();
-    likes.value = res.likes;
+async function getLikes(url_title: string) {
+  const res = await ky
+    .get(`/api/like?url_title=${url_title}`)
+    .json<{ likes: number }>();
+  console.log(res);
+  likes.value = res.likes;
+}
+
+async function like(url_title: string) {
+  const res = await ky
+    .post("/api/like", { json: { url_title: url_title } })
+    .json<{ likes: number }>();
+  likes.value = res.likes;
+}
+
+export default function likeArticle(props: { url_title: string }) {
+  if (IS_BROWSER) {
+    getLikes(props.url_title);
   }
 
   return (
     <div class={"flex items-center"}>
       <button
         class={"rounded-full p-2 hover:bg-gray-100 active:bg-gray-200"}
-        onClick={async () => like()}
+        onClick={async () => like(props.url_title)}
       >
         <IconHeart />
       </button>
--- routes/api/like.ts
+++ routes/api/like.ts
@@ -1,31 +1,48 @@
-import { HandlerContext } from "$fresh/server.ts";
+import { HandlerContext, Handlers } from "$fresh/server.ts";
 
 interface RequestExtended extends Request {
   json: () => Promise<{ url_title: string }>;
 }
 
-export const handler = async (
-  req: RequestExtended,
-  _ctx: HandlerContext,
-): Promise<Response> => {
-  // console.log((await req.json()).url_title);
+export const handler: Handlers = {
+  async GET(req, _ctx) {
+    const url = new URL(req.url);
 
-  const url_title = (await req.json()).url_title;
-  const kv = await Deno.openKv();
-  await kv
-    .atomic()
-    .mutate({
-      type: "sum",
-      key: ["articles", url_title, "likes"],
-      value: new Deno.KvU64(1n),
-    })
-    .commit();
+    const url_title = url.searchParams.get("url_title");
 
-  const val = await kv.get<Deno.KvU64>(["articles", url_title, "likes"]);
-  if (val.value) {
-    // console.log(Number(val.value.value));
-    return new Response(JSON.stringify({ likes: Number(val.value.value) }));
-  } else {
-    return new Response(JSON.stringify({ likes: 0 }));
-  }
+    if (url_title) {
+      const kv = await Deno.openKv();
+      const likes = await kv.get<Deno.KvU64>(["articles", url_title, "likes"]);
+      console.log(likes);
+      if (likes.value) {
+        return new Response(
+          JSON.stringify({ likes: Number(likes.value.value) }),
+        );
+      } else {
+        return new Response(JSON.stringify({ likes: 0 }));
+      }
+    } else {
+      return new Response(JSON.stringify({ likes: 0 }));
+    }
+  },
+
+  async POST(req, _ctx) {
+    const url_title = (await req.json()).url_title;
+    const kv = await Deno.openKv();
+    await kv
+      .atomic()
+      .mutate({
+        type: "sum",
+        key: ["articles", url_title, "likes"],
+        value: new Deno.KvU64(1n),
+      })
+      .commit();
+
+    const likes = await kv.get<Deno.KvU64>(["articles", url_title, "likes"]);
+    if (likes.value) {
+      return new Response(JSON.stringify({ likes: Number(likes.value.value) }));
+    } else {
+      return new Response(JSON.stringify({ likes: 0 }));
+    }
+  },
 };
